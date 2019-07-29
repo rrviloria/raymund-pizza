60fd6aaad683375f7eb091b07b379850de41e5c4
Raymund Viloria <raymund@epicode.nl>
2019-07-30 04:49:21 +0800

    Change container

--- run-seed.sh
+++ run-seed.sh
@@ -2,8 +2,8 @@
 # run-seed.sh
 
 echo "Eating all pizzas, yummy"
-docker exec -it pizza_web_1 pipenv run python manage.py flush --no-input
+docker exec -it raymundpizza_web_1 pipenv run python manage.py flush --no-input
 
 
 echo "Creating pizzas. please wait"
-docker exec -it pizza_web_1 pipenv run python seed.py
+docker exec -it raymundpizza_web_1 pipenv run python seed.py
--- run-test.sh
+++ run-test.sh
@@ -2,4 +2,4 @@
 # run-test.sh
 
 echo "Testing..."
-docker exec -it pizza_web_1 pipenv run python manage.py test
+docker exec -it raymundpizza_web_1 pipenv run python manage.py test
